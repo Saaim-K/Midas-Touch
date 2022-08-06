1567eb46aaf316744f29cb1e90505db66b73c21d
Saaim-K <saaimkhan221@gmail.com>
2022-08-07 03:01:29 +0500

    Artistic-Touch

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'red';
-ctx.fillRect(0, 0, 100, 100);
+// ctx.fillRect(0, 0, 100, 100);
 // const width = canvas.width;    // Assigning the canvas width
 // const height = canvas.height; // Assigning the canvas width 
 
