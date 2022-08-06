bb502b865b0ba7383082a6e946cc66a125e05f05
Saaim-K <saaimkhan221@gmail.com>
2022-08-07 02:42:54 +0500

    Artisitic-Touch

---
+++ app.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(0, 0, 100, 100);
+// const width = canvas.width;    // Assigning the canvas width
+// const height = canvas.height; // Assigning the canvas width 
+
+
+canvas.addEventListener("mousemove", (event) => {
+    console.log("click:",event);
+});
+
+document.addEventListener("mouseup", (event) => {
+    console.log(event);
+    document.body.style.backgroundColor = "white";
+});
