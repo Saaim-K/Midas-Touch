c98d2fff21925ee13587c0afe0cd4abd95730192
Saaim-K <saaimkhan221@gmail.com>
2022-08-07 03:14:21 +0500

    Artistic-Touch

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'red';
-// ctx.fillRect(0, 0, 100, 100);
+const ctx = canvas.getContext('2d'); //2D Drawing
+ctx.fillStyle = 'blue'; //Fill color
+ctx.fillRect(0, 0, 10, 10); //Draw a rectangle (x, y, width, height)
+// ctx.arc(40, 40, 20, 0, 2 * (Math.PI)); //Draw a circle (x, y, radius, startAngle, endAngle)
+// ctx.stroke();
 // const width = canvas.width;    // Assigning the canvas width
 // const height = canvas.height; // Assigning the canvas width 
 
