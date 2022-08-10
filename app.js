2076e54fc41e9e4d60b3080bf832845833bdf77e
Saaim-K <saaimkhan221@gmail.com>
2022-08-10 20:08:02 +0500

    Midas-Touch

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d'); //2D Drawing
-
+// --------------------Fill Rectangle--------------------
 ctx.fillStyle = 'blue'; //Fill color
 ctx.fillRect(0, 0, 10, 10); //Draw a rectangle (x, y, width, height)
+// --------------------Fill Rectangle--------------------
+
+
+// --------------------Stroke Rectangle--------------------
+ctx.strokeStyle = 'red'; //stroke color
+ctx.strokeRect(10, 20, 10, 10); //Draw a rectangle (x, y, width, height)
+// --------------------Stroke Rectangle--------------------
 
 
   
