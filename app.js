4f2d8d59f41cd7e95100c23676f4ecd33ead40fe
Saaim-K <saaimkhan221@gmail.com>
2022-08-10 15:00:40 +0500

    Midas Touch

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d'); //2D Drawing
+
 ctx.fillStyle = 'blue'; //Fill color
+ctx.fillRect(0, 0, 10, 10); //Draw a rectangle (x, y, width, height)
 
 
   
-// ctx.fillRect(0, 0, 10, 10); //Draw a rectangle (x, y, width, height)
 // ctx.arc(40, 40, 10, 0, (Math.PI)*2); //Draw a circle (x, y, radius, startAngle, endAngle)
 // ctx.stroke();
 // const width = canvas.width;    // Assigning the canvas width
